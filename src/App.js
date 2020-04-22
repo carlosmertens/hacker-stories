128f73bb91b29eb9552a43be76fc3bbad54da825
Carlos Mertens <mertenscarlos@yahoo.com>
2020-04-22 14:32:24 +0200

    Render primitive variable in JSX

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const welcome = {
+  name: 'Carlos',
+  lang: 'ReactJS'
+};
+
+function getLang(lang) {
+  return lang;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello {welcome.name} and welcome to {getLang(welcome.lang)}</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
