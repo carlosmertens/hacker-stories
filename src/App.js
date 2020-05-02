3d1f43c1fd3d21d865bd6490a05a33a4323235b2
Carlos Mertens <mertenscarlos@yahoo.com>
2020-05-02 10:59:51 +0200

    Use hook Effect to remember last search

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import React from 'react';
+import React from "react";
 
 const welcome = {
-  name: 'Carlos',
-  lang: 'ReactJS'
+  name: "Carlos",
+  lang: "ReactJS",
 };
 
 // To show rendering function in JSX
 const getLang = (lang) => {
   return lang;
-}
+};
 
 // Root component or parent component of the List, Search components
 function App() {
   const stories = [
     {
-      title: 'React',
-      url: 'http://reactjs.org/',
-      author: 'Jordan Walke',
+      title: "React",
+      url: "http://reactjs.org/",
+      author: "Jordan Walke",
       num_comments: 3,
       points: 4,
       objectID: 0,
     },
     {
-      title: 'Redux',
-      url: 'http//redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
+      title: "Redux",
+      url: "http//redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
       num_comments: 2,
       points: 5,
       objectID: 1,
@@ -32,15 +32,21 @@ function App() {
   ];
 
   // Initiate Search state
-  const [searchTerm, setSearchTerm] = React.useState('React');
+  const [searchTerm, setSearchTerm] = React.useState(
+    localStorage.getItem("search") || "React"
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem("search", searchTerm);
+  }, [searchTerm]);
 
   // Create callback handler
-  const handleSearch = event => {
-    setSearchTerm(event.target.value)
-  }
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
   //  Filter List with searched term (words)
-  const searchedStories = stories.filter(story =>
+  const searchedStories = stories.filter((story) =>
     story.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
@@ -49,7 +55,9 @@ function App() {
     <div>
       <h1>My Hacker Stories</h1>
       {/* Render primitive variables */}
-      <h3>Hello {welcome.name} and welcome to {getLang(welcome.lang)}!</h3>
+      <h3>
+        Hello {welcome.name} and welcome to {getLang(welcome.lang)}!
+      </h3>
 
       {/* Call Search component */}
       <Search search={searchTerm} onSearch={handleSearch} />
@@ -61,28 +69,33 @@ function App() {
 
       {/* Footnote */}
       <hr />
-      <p><i>Code by {name.getName()}</i></p>
+      <p>
+        <i>Code by {name.getName()}</i>
+      </p>
     </div>
   );
-};
+}
 
 // Create ListDisplay componet to render array
 // Child component of the App component and sibling component of Search
+// Use rest operatos (...) to separate and to map ID and items
 const ListDisplay = ({ list }) =>
-  list.map(({ objectID, ...item }) =>
-    <ItemList key={item.objectID} {...item} />);
+  list.map(({ objectID, ...item }) => (
+    <ItemList key={item.objectID} {...item} />
+  ));
 
 // Nested Destructuring the props
 const ItemList = ({ title, url, author, num_comments, points }) => (
   <div>
     <span>{title}-</span>
-    <span><a href={url}>{url}</a></span>
+    <span>
+      <a href={url}>{url}</a>
+    </span>
     <span> {author}-</span>
     <span>{num_comments}-</span>
     <span>{points}</span>
   </div>
 );
-
 
 // Create Search component
 // TODO: Refactor component from arrow function concise body to block body
@@ -90,12 +103,7 @@ const ItemList = ({ title, url, author, num_comments, points }) => (
 const Search = ({ search, onSearch }) => (
   <div>
     <label htmlFor="search">Search: </label>
-    <input
-      id="search"
-      type="text"
-      value={search}
-      onChange={onSearch}
-    />
+    <input id="search" type="text" value={search} onChange={onSearch} />
   </div>
 );
 
@@ -109,9 +117,9 @@ class Developer {
   getName() {
     return this.firstName + " " + this.lastName;
   }
-};
+}
 
-// Cretate an class instantiation 
-const name = new Developer('Carlos', 'Mertens');
+// Cretate an class instantiation
+const name = new Developer("Carlos", "Mertens");
 
 export default App;
